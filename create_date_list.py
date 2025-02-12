6b7fe798168960badb241b1f493f6366960cebbe
JP-YQN <jiangpeng274@outlook.com>
2025-02-12 16:01:09 +0800

    ds优化测试

--- create_date_list.py
+++ create_date_list.py
@@ -1,47 +1,37 @@
 # -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb, .py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.3'
-#       jupytext_version: 1.0.3
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
 
-import datetime 
+import datetime
 
 
-# + {"code_folding": []}
-def create_date_list(datestart = None,dateend = None): 
+def create_date_list(datestart=None, dateend=None, default_start='2016-01-01'):
     # 创建日期辅助表，返回为list
-    
-    if datestart is None: 
-        datestart = '2016-01-01' 
-    if dateend is None: 
-        dateend = datetime.datetime.now().strftime('%Y-%m-%d') 
-    
-    # 转为日期格式 
-    datestart=datetime.datetime.strptime(datestart,'%Y-%m-%d') 
-    dateend=datetime.datetime.strptime(dateend,'%Y-%m-%d') 
-    date_list = [] 
-    date_list.append(datestart.strftime('%Y-%m-%d')) 
-    while datestart<dateend: 
-        # 日期叠加一天 
-        datestart+=datetime.timedelta(days=+1) 
-        # 日期转字符串存入列表 
-        date_list.append(datestart.strftime('%Y-%m-%d')) 
-    
-    #print(date_list)
-    return date_list
 
+    try:
+        if datestart is None:
+            datestart = default_start
+        if dateend is None:
+            dateend = datetime.datetime.now().strftime('%Y-%m-%d')
 
-# -
+        # 转为日期格式
+        start_date = datetime.datetime.strptime(datestart, '%Y-%m-%d')
+        end_date = datetime.datetime.strptime(dateend, '%Y-%m-%d')
 
+        if start_date > end_date:
+            raise ValueError("Start date cannot be later than end date")
+
+        # 使用列表推导式生成日期列表
+        date_list = [
+            (start_date + datetime.timedelta(days=i)).strftime('%Y-%m-%d')
+            for i in range((end_date - start_date).days + 1)
+        ]
+
+        return date_list
+
+    except ValueError as e:
+        print(f"Invalid date format or logic error: {e}")
+        return []
+
+# 测试代码
 date_list = create_date_list('2019-03-04', '2019-05-04')
-type(date_list)
+print(type(date_list))
+print(date_list)
